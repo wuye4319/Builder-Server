38255357c689376e45504e89bf8a482c0761c6e6
杨君赫 <yangjunhe@yangjunhedeMacBook-Pro.local>
2019-06-27 22:02:45 +0800

    prod 生产模式

--- config/config.default.ts
+++ config/config.default.ts
@@ -29,13 +29,13 @@ export default (appInfo: EggAppInfo) => {
   // }
 
   // add your special config in here
-  const bizConfig = {
-    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-  };
+  // const bizConfig = {
+  //   sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  // };
 
   // the return config will combines to EggAppConfig
   return {
     ...config,
-    ...bizConfig,
+    // ...bizConfig,
   };
 };
--- config/config.prod.ts
+++
@@ -1,7 +0,0 @@
-import { EggAppConfig, PowerPartial } from 'egg';
-
-export default (appInfo) => {
-  const config: PowerPartial<EggAppConfig> = {};
-  config.keys = appInfo.name + '_1560331188782_4710';
-  return config;
-};
