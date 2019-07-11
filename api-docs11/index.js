46a0ae16a00f6df86899efb856608e193c6b8426
zhangxiang <zhangxiang@authine.com>
2019-07-11 23:18:04 +0800

    update

---
+++ api-docs11/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const express = require('express');
+const app = express();
+app.use('/', express.static('public'));
+app.listen(7001, () => {
+  console.log('[接口文档] 3001端口已开启');
+});
+123
